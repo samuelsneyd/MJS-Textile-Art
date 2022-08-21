ee7dca8b464762e9d0ddd5c4507beaa4617ebf18
samuel <samuelsneyd@gmail.com>
2022-08-21 16:40:21 +1200

    Init main nav bar routes, views, and HTML boilerplate

--- quilts/urls.py
+++ quilts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, AboutView, GalleryView, ResumeView, LinksView, ClassesView
 
 app_name = "quilts"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("gallery/", GalleryView.as_view(), name="gallery"),
+    path("about/", AboutView.as_view(), name="about"),
+    path("resume/", ResumeView.as_view(), name="resume"),
+    path("links/", LinksView.as_view(), name="links"),
+    path("classes/", ClassesView.as_view(), name="classes"),
 ]
--- quilts/views.py
+++ quilts/views.py
@@ -1,5 +1,31 @@
 from django.views.generic.base import TemplateView
+from .models import Quilt
 
 
 class IndexView(TemplateView):
     template_name = "quilts/index.html"
+
+
+class GalleryView(TemplateView):
+    template_name = "quilts/gallery.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["quilts"] = Quilt.objects.all()
+        return context
+
+
+class AboutView(TemplateView):
+    template_name = "quilts/about.html"
+
+
+class ResumeView(TemplateView):
+    template_name = "quilts/resume.html"
+
+
+class LinksView(TemplateView):
+    template_name = "quilts/links.html"
+
+
+class ClassesView(TemplateView):
+    template_name = "quilts/classes.html"
